cfd080cda5a787a2623bc25d51e5c00349c8d56f
ngdthinh97 <ngdthinh.97@gmail.com>
2020-07-15 14:27:20 +0700

    Used file xml config bean name

--- src/main/java/tng/springframework/joke/jokeapp/JokeappApplication.java
+++ src/main/java/tng/springframework/joke/jokeapp/JokeappApplication.java
@@ -2,8 +2,10 @@ package tng.springframework.joke.jokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-configuration.xml")
 public class JokeappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/tng/springframework/joke/jokeapp/config/ChuckConfiguration.java
+++ src/main/java/tng/springframework/joke/jokeapp/config/ChuckConfiguration.java
@@ -1,15 +1,15 @@
 package tng.springframework.joke.jokeapp.config;
 
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean; 
 import org.springframework.context.annotation.Configuration;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
-@Configuration
+//@Configuration  replace by chuck-configuration.xml file
 public class ChuckConfiguration {
 	
-	@Bean
+	//@Bean replace by chuck-configuration.xml file
 	public ChuckNorrisQuotes chuckNorrisQuotes() {
 		return new ChuckNorrisQuotes();
 	}
